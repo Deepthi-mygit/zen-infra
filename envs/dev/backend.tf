607d20975410c9b9c4693596360356c9c72c2ab3
Deepthi <deepashree975@gmail.com>
2026-05-29 16:39:20 +0530

    config: update backend bucket name for my account

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "zen-pharma-terraform-state-dpp-2026"
+    bucket = "zen-pharma-terraform-state-deepthi-mygit"
     key    = "envs/dev/terraform.tfstate"
     encrypt      = true
     use_lockfile = true   # S3 native locking
