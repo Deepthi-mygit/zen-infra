313473164fc9326322d8a042139b7a41e7dd75ff
Deepthi <deepashree975@gmail.com>
2026-06-26 16:55:34 +0530

    role added

--- modules/iam/github-actions-oidc.tf
+++ modules/iam/github-actions-oidc.tf
@@ -129,3 +129,43 @@ resource "aws_iam_role_policy_attachment" "github_actions_ci_policy_attachment" 
   role       = aws_iam_role.github_actions_ci.name
   policy_arn = aws_iam_policy.github_actions_ci_policy.arn
 }
+
+
+resource "aws_iam_role" "github_role" {
+  name = "github-actions-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [{
+      Effect = "Allow"
+
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github.arn
+      }
+
+      Action = "sts:AssumeRoleWithWebIdentity"
+
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        }
+
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:Deepthi-mygit/my-repo:*"
+        }
+      }
+    }]
+  })
+}
+
+
+
+
+
+# Attach permission
+
+resource "aws_iam_role_policy_attachment" "admin" {
+  role       = aws_iam_role.github_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
