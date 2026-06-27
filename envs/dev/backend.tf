85bc4ed4b298efe516582ca456d7cbe9014842ac
Deepthi <deepashree975@gmail.com>
2026-06-27 20:56:44 +0530

    role  added to Apply option

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -4,6 +4,7 @@ terraform {
   backend "s3" {
     bucket = "zen-pharma-terraform-state-deepthi-mygit"
     key    = "envs/dev/terraform.tfstate"
+    region = "us-east-1"
     encrypt      = true
     use_lockfile = true   # S3 native locking
   }
